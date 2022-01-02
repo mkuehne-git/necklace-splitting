9dbea589d1b2907d37ddda1864c6ddce55839316
mkuehne-git <mk314git@gmail.com>
2022-01-02 22:35:22 +0100

    Initial commit

---
+++ src/shaders/vertex.glsl
@@ -0,0 +1,66 @@
+#include uniform;
+#include varying;
+
+struct SphereData {
+    vec3 p;
+    float octant;
+    bool valid;
+};
+
+SphereData displace_octant(vec3 p, float offset) {
+    SphereData sd;
+    sd.valid = true;
+    if(offset == 0.0) {
+        sd.p = p;
+    } else if(p.x >= 0.0 && p.y >= 0.0 && p.z >= 0.0) {
+        // 000
+        sd.p = p + vec3(offset, offset, offset);
+        sd.octant = 1.0;
+    } else if(p.x > 0.0 && p.y > 0.0 && p.z < 0.0) {
+        // 001
+        sd.p = p + vec3(offset, offset, -offset);
+        sd.octant = 1.0;
+    } else if(p.x > 0.0 && p.y < 0.0 && p.z > 0.0) {
+        // 010
+        sd.p = p + vec3(offset, -offset, offset);
+        sd.octant = 2.0;
+    } else if(p.x > 0.0 && p.y < 0.0 && p.z < 0.0) {
+        // 011
+        sd.p = p + vec3(offset, -offset, -offset);
+        sd.octant = 3.0;
+    } else if(p.x < 0.0 && p.y > 0.0 && p.z > 0.0) {
+        // 100
+        sd.p = p + vec3(-offset, offset, offset);
+        sd.octant = 4.0;
+    } else if(p.x < 0.0 && p.y > 0.0 && p.z < 0.0) {
+        // 101
+        sd.p = p + vec3(-offset, offset, -offset);
+        sd.octant = 5.0;
+    } else if(p.x < 0.0 && p.y < 0.0 && p.z > 0.0) {
+        // 110
+        sd.p = p + vec3(-offset, -offset, offset);
+        sd.octant = 6.0;
+    } else if(p.x < 0.0 && p.y < 0.0 && p.z < 0.0) {
+        // 111
+        sd.p = p + vec3(-offset, -offset, -offset);
+        sd.octant = 7.0;
+    } else {
+        sd.p = p;
+        sd.valid = false;
+    }
+    return sd;
+}
+
+void main() {
+    // Calculate vertex shader output
+    SphereData sd_shader = displace_octant(position, u_offset_sphere_octant);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(sd_shader.p, 1.0);
+
+    v_pos = position / u_scaled_radius;
+
+    // Calculate internal output and add displacement to sphere octants
+    SphereData sd_out = displace_octant(v_pos, u_offset_sphere_octant / u_scaled_radius.x);
+    v_sphereData_p = sd_out.p;
+    v_sphereData_octant = sd_out.octant;
+    v_sphereData_valid = sd_out.valid ? 1.0 : 0.0;
+}
